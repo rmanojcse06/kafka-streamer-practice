72873d3538abae05d675449685a9b73f4b7624a5
Manoj Ravikumar <manoj.ravikumar@hpe.com>
2025-12-03 21:03:15 +0530

    project init

---
+++ mongodb-listener/src/main/java/edu/man/mongo/listener/Main.java
@@ -0,0 +1,15 @@
+package edu.man.mongo.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.info("Starting mongodb-listener application");
+        SpringApplication.run(Main.class, args);
+    }
+}
