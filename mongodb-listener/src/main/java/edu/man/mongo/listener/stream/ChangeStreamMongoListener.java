72af039a48d7280d6c37037006549112498c3d45
Manoj Ravikumar <manoj.ravikumar@hpe.com>
2026-01-02 14:16:40 +0530

    mongotemplate to kafkatemplate

--- mongodb-listener/src/main/java/edu/man/mongo/listener/stream/ChangeStreamMongoListener.java
+++ mongodb-listener/src/main/java/edu/man/mongo/listener/stream/ChangeStreamMongoListener.java
@@ -3,20 +3,19 @@ package edu.man.mongo.listener.stream;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.ChangeStreamOptions;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class ChangeStreamMongoListener {
-    // Inject the bean via the constructor (best practice)
-    private final ReactiveMongoTemplate reactiveMongoTemplate;
-
-    // Use 'final' for immutable field and remove @Autowired
-    public ChangeStreamMongoListener(ReactiveMongoTemplate reactiveMongoTemplate) {
-        this.reactiveMongoTemplate = reactiveMongoTemplate;
-    }
+    @Autowired
+    private ReactiveMongoTemplate reactiveMongoTemplate;
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
 
     @PostConstruct
     public void bootstrapListener() {
@@ -51,8 +50,14 @@ public class ChangeStreamMongoListener {
                 .doOnError(err -> log.error("Change stream error", err))
                 .retry()  // <--- IMPORTANT: automatic restart
                 .subscribe(event -> {
-                    Document rawData = event.getBody();
-                    log.info("MongoDB rawData: {} and operation: {}", event.getBody(), event.getOperationType() );
+                    Document body = event.getBody();
+                    if (body != null) {
+                        log.info("MongoDB rawData: {} and operation: {}", event.getBody(), event.getOperationType());
+                        String jsonPayload = body.toJson();
+                        String kafkaKey = event.getResumeToken().toString();
+                        log.info("Pushing to Kafka with key {} and payload {}", kafkaKey, jsonPayload);
+                        kafkaTemplate.send("man.event.stream.start", kafkaKey, jsonPayload);
+                    }
         });
         log.info("✔ MongoDB change stream listener registered for inventory collection");
     }
